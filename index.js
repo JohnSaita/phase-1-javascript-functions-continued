08d93b67af2065534443908283bb87c85d991a6d
John <jsaita48@gmail.com>
2022-08-16 10:38:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(fun){
+  if(fun== null){
+    return "This Saturday, I want to roller-skate!";
+  }else{
+    return `This Saturday, I want to ${fun}!`
+  }
+}
+function mondayWork(work){
+  return work == null? "This Monday, I will go to the office.": `This Monday, I will ${work}.`;
+}
+function wrapAdjective(adjective){
+ return function(string){
+    return `You are ${adjective}${string}${adjective}!`
+ }
+}
+
